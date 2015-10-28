731ad76018d4b01a96abd7ae01721f6d3f62f15a
shery15 <382254994@qq.com>
2015-10-28 21:39:49 +0800

    1028

--- js/index.js
+++ js/index.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
-	$('.blog').click(function() {
-		alert("建设中");
-	});
+	var draw = function() {
+		var canvas = document.getElementById("avatar");
+   		if (canvas.getContext) {
+	        var logo = canvas.getContext("2d");
+
+	        logo.fillStyle = "rgb(255,255,255)";
+	        logo.fillRect (0, 0, 100, 200);
+
+	        logo.fillStyle = "rgb(34, 195, 169)";
+	        logo.fillRect (100, 0, 100, 200);
+
+	        logo.lineWidth = 3;
+	        logo.lineCap = "round";
+	        logo.lineJoin= "miter";
+	        logo.beginPath();
+	        logo.moveTo(100,158);
+	        logo.lineTo(122,130);
+	        logo.lineTo(110,84);/*折点*/
+			logo.lineTo(124,60);
+			logo.lineTo(164,46);
+	        // logo.closePath();
+	        logo.strokeStyle = "rgb(255,255,255)"
+	        logo.stroke();
+
+	        logo.beginPath();
+	        logo.moveTo(110,84);
+	        logo.lineTo(146,72);
+	        logo.strokeStyle = "rgb(255,255,255)"
+	        logo.stroke();
+
+	        logo.beginPath();
+	        logo.moveTo(100,158);
+	        logo.lineTo(78,130);
+	        logo.lineTo(90,84);/*折点*/
+			logo.lineTo(76,60);
+			logo.lineTo(36,46);
+	        // logo.closePath();
+	        logo.strokeStyle = "rgb(34,195,169)"
+	        logo.stroke();
+
+	        logo.beginPath();
+	        logo.moveTo(90,84);
+	        logo.lineTo(54,72);
+	        logo.strokeStyle = "rgb(34,195,169)"
+	        logo.stroke();
+     	} else {
+     		$('#container').append('<img  id="avatar" src="images/avatar.png">');
+     	}
+	};
 });
